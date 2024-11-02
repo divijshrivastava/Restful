eee4a78e3a6a42224f5c90bd4e2709cc90774d63
Divij <divij.shrivastava@gmail.com>
2024-11-03 04:56:10 +0530

    initialized project

---
+++ restful/src/test/java/tech/divij/restful/RestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.divij.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
